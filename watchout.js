01787e346956712ff3f22eb386f76e6ddb0f5a3d
devmeyster <arthurfbi@yahoo.com>
2015-05-20 13:27:33 -0700

    updated css and html of game components

--- watchout.js
+++ watchout.js
@@ -1,5 +1,4 @@
 
-var astroid = "./asteroid.png";
 var width = 200;
 var height = 200;
 var score = d3.select(".current").select('span');
@@ -12,8 +11,8 @@ var createEnemies = function(n){
   return _.range(0, n).map(function(i){
     return {
       'id': i,
-      'x': 10+Math.random()* 710,
-      'y': 10+Math.random()* 500
+      'x': 10+Math.random()* 1500,
+      'y': 10+Math.random()* 1200
     };
   });
 };
@@ -25,10 +24,10 @@ d3.select ('body')
  .append ('defs')
  .attr ({ id : 'mdef'})
  .append ('pattern')
- .attr ({ id : 'image', x : 0, y : 0, height: 40, width : 40})
+ .attr ({ id : 'image', x : 0, y : 0, height: 35, width : 35})
  .append ('image')
- .attr ({ x : 0, y : 0, width : 40, height : 40,
-            'xlink:href' : './asteroid.png'});
+ .attr ({ x : 0, y : 0, width : 33, height : 33,
+            'xlink:href' : './asteroidgif.gif'});
 
  d3.select ('body')
   .append ('svg')
@@ -39,42 +38,43 @@ d3.select ('body')
   .attr ({ id : 'playerImage', x : 0, y : 0, height: 40, width : 40})
   .append ('image')
   .attr ({ x : 0, y : 0, width : 40, height : 40,
-             'xlink:href' : './yoda.gif'});
+             'xlink:href' : './spaceship.png'});
 
 
 
 
 
-var board = d3.select("body")			                //selecting the <body> 
-            .append("svg")				                //appending SVG element 
-            .attr("height", "500px")	                //setting board height
-            .attr("width", "750px");	                //setting board height
-
+var board = d3.select("body")			                    //selecting the <body> 
+            .append("svg")	                          //appending SVG element
+            .attr("class", "board")			                     
+            .attr("height", "680px")	                //setting board height
+            .attr("width", "1250px");	                //setting board height
+            
 
 var enemies = board.selectAll('circle')                 //select the (non-existant) SVG circle element
-                   .data(createEnemies(30));            //pass in the data from the createEnemies() function
-                   .enter().append("circle")			//append circle to all the "extra" data elements
-                   .attr("class", "enemy")				//assign class "enemy"
-                   .attr("r", 10)						//set the circle radius to 10
+                   .data(createEnemies(30))             //pass in the data from the createEnemies() function
+                   .enter().append("circle")			      //append circle to all the "extra" data elements
+                   .attr("class", "enemy")			     	  //assign class "enemy"
+                   .attr("r", 15)						            //set the circle radius to 10
                    .attr("cx", function(d){return d.x})	//assign the x coordinate
                    .attr("cy", function(d){return d.y})	//assign the y coordinate
                    .style("fill", "url(#image)")       	//set the enemy's background image
                    .style("stroke", "black")     
-                   .style("stroke-width", 0.25);
+                   .style("stroke-width", 0.0001);
 
 var player = board.append("circle")                      //append circle element to the board 
                   .attr("class", "player")	             //set class to "player"
-                  .attr("r", 20)			             //set radius to 20
-                  .attr("cx", 150)			             //assign the x coordinate
-                  .attr("cy", 150)			             //assign the y coordinate
+                  .attr("r", 20)			                   //set radius to 20
+                  .attr("cx", 150)			                 //assign the x coordinate
+                  .attr("cy", 150)			                 //assign the y coordinate
                   .style("fill", "url(#playerImage)");	 //set the player's background image
 
 var move = function(){
-  board.selectAll('.enemy')								 //select all circles with an "enemy" class
-       .data(createEnemies(30));						 //invoke createEnemies() to pass in new coordinates
- 	   .transition().duration(1500)						 //implement D3 transitions with duration of 1.5 seconds
- 	   .attr("cx", function(d){return d.x})				 //update the x coordinate
-       .attr("cy", function(d){return d.y});		     //update the y coordinate
+  board.selectAll('.enemy')								               //select all circles with an "enemy" class
+       .data(createEnemies(30))						               //invoke createEnemies() to pass in new coordinates
+ 	     .transition().duration(1500)						           //implement D3 transitions with duration of 1.5 seconds
+ 	     .attr("cx", function(d){return d.x})		           //update the x coordinate
+       .attr("cy", function(d){return d.y});	           //update the y coordinate
 };
 
 
@@ -107,7 +107,9 @@ var detector = function(){
         high.text(currentScore);
         highScore = currentScore;
       }
+      alert("Game Over! Your score: " + currentScore + ". Try Again?");
       currentScore = 0;
+      player.attr("cx", 150).attr("cy", 150);
     }
     score.text(currentScore);
     currentScore++;
